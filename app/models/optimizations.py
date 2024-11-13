43e6e0d276a392ff984f09724c9a6097378c6905
santiagog1025 <santiagog102501@gmail.com>
2024-11-12 21:55:38 -0500

    cambios generales en todos los archivos

--- app/models/optimizations.py
+++ app/models/optimizations.py
@@ -1,60 +1,123 @@
-import os
-from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-from models.embeddings_model import crear_vector_store
-from typing import List, Dict
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains import LLMChain
+import os
+import io
 
-def optimizar_cv_rag(texto_cv: str, job_position: str, documentos: List[str]) -> Dict[str, List[str]]:
+def leer_pdf(file) -> str:
     """
-    Optimiza el currículum en función de un cargo específico, sugiriendo mejoras usando RAG.
+    Lee un archivo PDF usando LangChain y extrae su contenido como texto.
 
     Args:
-        texto_cv (str): Texto extraído del currículum.
-        job_position (str): Cargo al cual se quiere postular.
-        documentos (List[str]): Lista de textos/documentos para contexto adicional.
+        file (UploadFile): Archivo PDF cargado.
 
     Returns:
-        dict: Diccionario con sugerencias de optimización para mejorar el CV.
+        str: Texto extraído del PDF.
     """
-    # Crear la base de datos de embeddings con Chroma
-    vector_store = crear_vector_store(documentos)
-
-    # Configurar el modelo de lenguaje y la plantilla de prompt
-    llm = ChatGroq(model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY"))
-    prompt_template = PromptTemplate(
-        input_variables=["context", "cv_text", "job_position"],
-        template=(
-  "Currículum:\n"
-    "{context}\n\n"
-    "Puesto objetivo:\n"
-    "{job_position}\n\n"
-    "Instrucciones:\n"
-    "1. Analiza el currículum proporcionado en relación con los requisitos y responsabilidades del puesto de trabajo descrito.\n"
-    "2. Identifica las competencias clave, habilidades y experiencias requeridas para el puesto.\n"
-    "3. Proporciona una lista de sugerencias de mejora para optimizar el currículum, destacando cómo resaltar las habilidades más relevantes, experiencias previas y logros de manera que se alineen mejor con las expectativas del empleador.\n"
-    "4. Asegúrate de que las sugerencias estén enfocadas en mejorar la claridad, relevancia y impacto del perfil del candidato para que se ajuste al puesto de trabajo objetivo."
-        )
-    )
-
-    # Crear la cadena RAG con LangChain
-    cadena_rag = RetrievalQA(
-        retriever=vector_store.as_retriever(),
-        llm=llm,
-        prompt=prompt_template
-    )
-
-    # Generar el contexto y hacer la consulta al modelo LLM
-    context = vector_store.similarity_search(texto_cv, k=5)
-    context_text = " ".join([doc.page_content for doc in context])  # Concatenar el contexto recuperado
-
-    # Formatear el prompt y obtener sugerencias
-    prompt_final = prompt_template.format(context=context_text, cv_text=texto_cv, job_position=job_position)
-
     try:
-        # Llamar al modelo de lenguaje con el prompt y generar sugerencias
-        respuesta = cadena_rag({"question": prompt_final})
-        sugerencias = respuesta['answer'].split("\n")
-        return {"sugerencias": [s.strip() for s in sugerencias if s.strip()]}  # Filtrar sugerencias vacías
+        loader = PyPDFLoader(file)
+        
+        # Cargar el documento
+        documento = loader.load()
+        return documento
+
     except Exception as e:
-        raise ValueError(f"Error al generar sugerencias de optimización con RAG: {str(e)}")
+        raise ValueError(f"Error al leer el archivo PDF: {str(e)}")
+
+# Configuración de RAG
+def configurar_rag(documento, puesto) -> RetrievalQA:
+    """
+    Configura la cadena RAG para el modelo de optimización de CV.
+
+    Args:
+        documentos (list): Lista de textos de referencia para el almacenamiento vectorial.
+
+    Returns:
+        RetrievalQA: Cadena RAG configurada.
+    """
+    # Configurar embeddings y vector store
+    documento = leer_pdf(documento)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.split_documents(documento)
+    
+    # Crear embeddings
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    vector_store = FAISS.from_documents(docs, embeddings)
+    
+    # Configurar el modelo de lenguaje y el prompt
+    prompt_template =f"""
+            "Lee el siguiente currículum y extrae las partes más importantes relacionadas con la consulta del usuario. Proporciona detalles clave como la experiencia laboral, habilidades técnicas, formación académica, logros, certificaciones y cualquier otra información relevante que responda directamente a la consulta. Asegúrate de que la información extraída sea específica y organizada, y destaca las secciones que se ajusten mejor a lo que está buscando el usuario.
+
+            Consulta del usuario: {puesto}
+
+
+            Responde con la información más relevante relacionada con la consulta que debería estar relacionada al puesto de trabajo al que aspira el usuario. ten en cuenta que tienes un máximo de 1000 tokens para responder"
+    """
+    # Crear la cadena RAG
+    llm = ChatGroq(model_name="mixtral-8x7b-32768", groq_api_key="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", temperature= 0, max_tokens=1000)
+    rag_chain = RetrievalQA.from_chain_type(
+        retriever=vector_store.as_retriever(),
+        return_source_documents=True,
+        llm=llm
+    )
+    response = rag_chain.invoke(prompt_template)
+    return response['result']
+
+# Definir un agente que reciba la salida de la herramienta RAG y genere una respuesta en español
+def configurar_agente_rag(documento, puesto):
+    """
+    Configura el agente LangChain que toma la respuesta de la herramienta RAG y genera una respuesta optimizada en español.
+
+    Args:
+        documento (str): Ruta del documento PDF.
+        puesto (str): Nombre del puesto de trabajo objetivo.
+    
+    Returns:
+        str: Respuesta generada por el agente en español.
+    """
+    # Obtener la respuesta de la herramienta RAG
+    resultado_rag = configurar_rag(documento, puesto)
+    
+    # Definir un prompt para el agente LangChain
+    prompt_template = f"""
+    Rol: Eres un asistente virtual especializado en optimización de currículums, utilizando técnicas avanzadas para mejorar la presentación y alineación del perfil del candidato con los requisitos de un puesto específico. Tu tarea es analizar el currículum proporcionado por el usuario para el puesto de trabajo {puesto} y ofrecer recomendaciones detalladas para optimizar el perfil del candidato.
+
+    Información proporcionada:
+    
+    Análisis del currículum: {resultado_rag}
+    Puesto de trabajo: {puesto}
+    Instrucciones:
+    
+    Revisa cuidadosamente el currículum y compáralo con los requisitos del puesto proporcionado.
+    Utiliza técnicas de optimización de currículums para mejorar la efectividad del perfil del candidato. Algunas técnicas incluyen:
+    Resaltar logros cuantificables.
+    Asegurar la inclusión de palabras clave relevantes para el puesto.
+    Optimizar la estructura para facilitar la lectura y destacar las secciones más importantes.
+    Usar un formato atractivo y profesional que facilite el escaneo rápido por los reclutadores.
+    Proporciona recomendaciones específicas para mejorar el currículum, basándote en las áreas que necesitan mayor ajuste para alinearse mejor con el puesto de trabajo.
+    Haz énfasis en los siguientes aspectos:
+    Experiencia laboral: Sugiere cómo resaltar logros clave o responsabilidades relevantes, utilizando verbos de acción y cifras cuantificables.
+    Habilidades técnicas: Identifica habilidades faltantes que podrían ser incluidas, si el candidato las posee, y cómo presentarlas de manera efectiva.
+    Educación y formación: Señala cómo resaltar títulos o certificaciones relevantes para el puesto, destacando cualquier formación adicional pertinente.
+    Logros y competencias adicionales: Recomendaciones sobre cómo mejorar la sección de logros y habilidades blandas, utilizando ejemplos concretos cuando sea posible.
+    Personaliza la respuesta si el candidato menciona su nombre, utilizando un tono amigable y motivacional.
+    Objetivo: Ayudar al candidato a mejorar su currículum para aumentar sus posibilidades de éxito en la postulación al puesto de {puesto}, utilizando técnicas de optimización de currículums que lo hagan destacar entre otros postulantes."""
+
+
+
+    # Crear un modelo de lenguaje de LangChain 
+    llm = ChatGroq(model="gemma2-9b-it", groq_api_key="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", max_tokens=1000) 
+
+    # Crear un LLMChain con el prompt y el modelo
+    llm_chain = LLMChain(prompt=PromptTemplate.from_template(prompt_template), llm=llm)
+
+    # Ejecutar el agente para generar la respuesta
+    respuesta = llm_chain.run(prompt= puesto)
+    
+    return respuesta
+
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,10 @@
 # app/routes.py
-
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from utils import leer_pdf
-from models.optimizations import optimizar_cv_rag
+from models.optimizations import configurar_agente_rag
 
 # Crear el enrutador
 router = APIRouter()
 
-@router.post("/upload_cv/")
-async def upload_cv(file: UploadFile = File(...)):
-    """
-    Endpoint para subir un archivo de currículum en PDF y extraer el texto.
-    """
-    try:
-        # Leer el contenido del archivo PDF
-        texto_cv = leer_pdf(file)
-        return {"texto_cv": texto_cv}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al procesar el PDF: {str(e)}")
 
 
 @router.post("/optimize_cv/")
@@ -29,11 +16,9 @@ async def optimize_cv(file: UploadFile = File(...), job_position: str = ""):
         raise HTTPException(status_code=400, detail="Debe especificar el cargo deseado.")
 
     try:
-        # Leer el contenido del archivo PDF
-        texto_cv = leer_pdf(file)
 
         # Optimizar el currículum en función del cargo deseado
-        optimizaciones = optimizar_cv_rag(texto_cv, job_position)
+        optimizaciones = configurar_agente_rag(file.file, job_position)
         return {"optimizaciones": optimizaciones}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al optimizar el CV: {str(e)}")
